ce4943a9358e35bef469ea828b758cdbf96b384c
AEssadi91 <120156769+AEssadi91@users.noreply.github.com>
2023-10-01 21:47:37 -0600

    Update page.js
    week3

--- app/week3/page.js
+++ app/week3/page.js
@@ -3,7 +3,7 @@ import ItemList from "./item-list";
 export default function Page() {
   return (
     <div className="bg-indigo-950">
-      <h2 className="text-2xl font-bold">Shopping List</h2>
+      <h2 className="text-3xl font-bold">Shopping List</h2>
       <ItemList />
     </div>
   );
