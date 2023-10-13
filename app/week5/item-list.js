f639573939e7bd1d45b4a67364cabc95d1e01e3b
AEssadi91 <120156769+AEssadi91@users.noreply.github.com>
2023-10-13 09:42:27 -0600

    week5
    week5

--- app/week5/item-list.js
+++ app/week5/item-list.js
@@ -7,68 +7,59 @@ export default function ItemList() {
   const [sortBy, setSortBy] = useState("name");
   const [groupedCategory, setGroupedCategory] = useState(false);
 
-  items.sort((a, b) => {
-    if (sortBy == "name") {
-      return a.name.localeCompare(b.name);
-    } else if (sortBy == "category") {
-      return a.category.localeCompare(b.category);
-    }
-    return 0; 
-  });
-  
+  items.sort((a, b) =>  sortBy === "name"  ? a.name.localeCompare(b.name)
+      : sortBy === "category" ? a.category.localeCompare(b.category) : 0
+  );
+
   const groupedItems = items.reduce((acc, item) => {
-    if (!acc[item.category]) {
-      acc[item.category] = [];
-    }
+    acc[item.category] = acc[item.category] || [];
     acc[item.category].push(item);
     return acc;
   }, {});
 
   const sortedCategories = Object.keys(groupedItems).sort();
-  sortedCategories.forEach(category => {
+  sortedCategories.forEach((category) => {
     groupedItems[category].sort((a, b) => a.name.localeCompare(b.name));
   });
 
   return (
     <div>
-        <label for= "sort">Sort by:</label>
+      <label for="sort">Sort by:</label>
       <button
         className="p-1 m-2 w-28"
         onClick={() => setSortBy("name")}
-        style={{ backgroundColor: sortBy == "name" ? "#0284c7" : "#0c4a6e"  }}
+        style={{ backgroundColor: sortBy == "name" ? "#0284c7" : "#0c4a6e" }}
       >
         Name
       </button>
       <button
         className="p-1 m-2 w-28"
         onClick={() => setSortBy("category")}
-        style={{ backgroundColor: sortBy == "category" ? "#0284c7" : "#0c4a6e" }}
+        style={{
+          backgroundColor: sortBy == "category" ? "#0284c7" : "#0c4a6e",
+        }}
       >
         Category
       </button>
 
-      <button 
+      <button
         className="p-1 m-2 w-28"
-        onClick={() => setGroupedCategory(!groupedCategory)} 
+        onClick={() => setGroupedCategory(!groupedCategory)}
         style={{ backgroundColor: groupedCategory ? "#0284c7" : "#0c4a6e" }}
       >
         Grouped Category
       </button>
 
-      {groupedCategory ? (
-        sortedCategories.map(category => (
-          <div key={category}>
-            <h2 className="capitalize">{category}</h2>
-            {groupedItems[category].map(item => (
-              <Item key={item.id} item={item} />
-            ))}
-          </div>
-        ))
-      ) : (
-        items.map(item => <Item key={item.id} item={item} />)
-      )}
+      {groupedCategory
+        ? sortedCategories.map((category) => (
+            <div key={category}>
+              <h2 className="capitalize">{category}</h2>
+              {groupedItems[category].map((item) => (
+                <Item key={item.id} item={item} />
+              ))}
+            </div>
+          ))
+        : items.map((item) => <Item key={item.id} item={item} />)}
     </div>
   );
-};
-
-
+}
